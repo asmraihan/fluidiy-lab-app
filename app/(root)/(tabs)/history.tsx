c15b46559f53647ab08ea6c7e2be1a136b86611e
asmraihan <asmraihanctg14@gmail.com>
2025-05-09 23:45:06 +0200

    fix: update sign-out logic and improve error handling in HistoryScreen

--- app/(root)/(tabs)/history.tsx
+++ app/(root)/(tabs)/history.tsx
@@ -1,19 +1,35 @@
 import { View, Text, StatusBar, Image, StyleSheet, Platform, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useUser } from "@clerk/clerk-expo";
-import { useAuth } from "@clerk/clerk-expo";
+import { useClerk } from '@clerk/clerk-expo'
+import * as Linking from 'expo-linking'
+
 import { router } from "expo-router";
 import { icons, images } from "@/constants";
 import CustomButton from '@/components/CustomButton';
 
 export default function HistoryScreen() {
   const { user } = useUser();
-  const { signOut } = useAuth();
+  const { signOut } = useClerk()
 
-  const handleSignOut = () => {
-    signOut();
-    router.replace("/(auth)/sign-in");
-  };
+  // const handleSignOut = () => {
+  //   signOut();
+  //   router.replace("/(auth)/sign-in");
+  // };
+
+    const handleSignOut = async () => {
+  console.log(user)
+
+    try {
+      await signOut()
+      // Redirect to your desired page
+      Linking.openURL(Linking.createURL('/(auth)/sign-in'))
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2))
+    }
+  }
 
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
@@ -24,7 +40,7 @@ export default function HistoryScreen() {
           <Text className="text-xl font-JakartaBold text-center  text-white">Log out</Text>
           <TouchableOpacity
             onPress={handleSignOut}
-            className=""
+            className="px-6 py-2"
           >
             <Image source={icons.out} className="w-4 h-4" />
           </TouchableOpacity>
